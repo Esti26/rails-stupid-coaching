5624c452642ae6e0ed303ed023f91d5463c52038
Estibaliz Lorenzo <esti_2612@hotmail.com>
2023-05-22 14:06:08 +0200

    solves styling issues

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
+# The QuestionsController is in charge of our stupid questions
 class QuestionsController < ApplicationController
-  def ask
-  end
+  def ask; end
 
   def answer
-    answers = ["Great!", "What a silly question", "I don't care, get dressed and go to work"]
+    answers = ['Great!', 'What a silly question', "I don't care, get dressed and go to work"]
     @question = params[:question]
-    case @question
-    when @question == 'I am going to work'
-      @answer = answers[0]
-    when @question.slice(-1) == '?'
-      @answer = answers[1]
-    else
-      @answer = answers[2]
-    end
+    @answer = case @question
+              when @question == 'I am going to work'
+                answers[0]
+              when @question.slice(-1) == '?'
+                answers[1]
+              else
+                answers[2]
+              end
   end
 end
